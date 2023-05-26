2664dd4dc73d87ac0e6bb14080a5a38abaae09be
weitf <quiet191@163.com>
2023-05-26 15:17:47 +0800

    update init

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # vue-flip-number-card
+
+a flip number card component for vue.
+
+follow the lead of  [干货满满!如何优雅简洁地实现时钟翻牌器(支持JS/Vue/React)](https://juejin.im/post/5dd9490a6fb9a07a961d11e7)
+
+## demo
+
+![avatar](/demo.gif)
+
+## usage
+
+```html
+<filp-number :value="value" :speed="700"></filp-number>
+```
+```js
+import FilpNumber from '@/components/FlipNumber'
+```
+
+```js
+props: {
+  value: {
+    type: String,
+    required: true,
+    default: '0'
+  },
+  speed: {
+    type: [Number, String],
+    default: 600
+  }
+}
+```
+
+ value:
+ could be like '00000.00'、'0012' and so on for placeholder
+
+ speed:
+ could not be less than 600.
+
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <filp-number :value="value" :speed="700"></filp-number>
+    <button @click="generate" style="margin-top:20px;">generate</button>
+    <div style="margin-top:20px;">
+      {{value}}
+    </div>
+    <button @click="add" style="margin-top:20px;">add</button>
+    <button @click="abstract" style="margin-top:20px;">abstract</button>
+  </div>
+</template>
+
+<script>
+import FilpNumber from '@/components/FlipNumber'
+
+export default {
+  name: 'app',
+  components: {
+    FilpNumber
+  },
+  data () {
+    return {
+      value: '0000.00'
+      // value: '0'
+    }
+  },
+  methods: {
+    generate () {
+      // this.value = (Math.random() * 100000).toFixed(2)
+      this.value = (Math.random() * 10000).toFixed(2)
+    },
+    add () {
+      this.value *= 1
+      this.value += 9
+    },
+    abstract () {
+      this.value *= 1
+      this.value -= 9
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  margin: 100px 200px;
+}
+</style>
